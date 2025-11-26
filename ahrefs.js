c5f13089a88bfdafcd04096c3a80d8dcb1e9a94e
escafati <scafatieugenio@gmail.com>
2025-11-26 15:04:34 -0300

    Add Ahrefs analytics script to ahrefs.js (#20)

---
+++ ahrefs.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://analytics.ahrefs.com/analytics.js';
+  script.setAttribute('data-key', '4xZsy8g4ancQqnN0pZ6RLA');
+  script.async = true;
+  document.head.appendChild(script);
+})();
